f55ca7d41f23cb77ee7fe20d6e43b5a46bb8dbcf
LuisOtavioOliveira <lotaviooliveira12@gmail.com>
2023-10-12 20:00:59 -0300

    Localstorage

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const getUsername = () => {
     const name = document.getElementById('inputname').value
     console.log(name)
+    localStorage.setItem('name', name);
 
 }
 
 const replaceName = () => {
     const place = document.getElementById('welcomename')
-    place.innerText = `Olá, ${sessionStorage.getItem('name')}!`
+    place.innerText = `Olá, ${localStorage.getItem('name')}!`
 }
 
 
